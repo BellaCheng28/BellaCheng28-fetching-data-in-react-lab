a72785a7e751b0b3d44d4065866e60e8847a4531
Your Name <you@example.com>
2024-12-09 15:55:28 -0500

    fetching Data

---
+++ src/components/WeatherSearch.jsx
@@ -0,0 +1,15 @@
+const WeatherSearch = ({ setLocation, setTemp, getWeather }) => {
+  const handleChange = (event) => {
+    setLocation(event.target.value);
+    setTemp("");
+  };
+
+  return (
+    <div>
+      <input name="location" onChange={handleChange} />
+      <button onClick={getWeather}>Get Weather</button>
+    </div>
+  );
+};
+
+export default WeatherSearch;
